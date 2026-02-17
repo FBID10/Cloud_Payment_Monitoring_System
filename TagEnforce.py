8c25d034263da8373078722c5e40274cc4be5190
Induranga FBD <83300979+FBID10@users.noreply.github.com>
2026-02-17 14:27:52 +0000

    Added violater Rule

--- TagEnforce.py
+++ TagEnforce.py
@@ -10,4 +10,26 @@ class TagEnforcer:
             tag_dict[tag['Key']] = tag['Value']
         return tag_dict
 
-  
+    def find_violators(self, instances):
+        """Identifies which instances are missing required tags."""
+        violators = []
+
+        for instance in instances:
+            instance_id = instance['InstanceId']
+            # Convert the AWS tag list into a searchable dictionary
+            readable_tags = self._get_readable_tags(instance.get('Tags', []))
+            
+            missing = []
+            for tag in self.required_tags:
+                if tag not in readable_tags:
+                    missing.append(tag)
+            
+            # If we found missing tags, record this instance as a violator
+            if missing:
+                violators.append({
+                    "InstanceId": instance_id,
+                    "MissingTags": missing,
+                    "LaunchTime": instance['LaunchTime'].strftime("%Y-%m-%d %H:%M:%S")
+                })
+        
+        return violators
