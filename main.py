68376de69628bb7460b841257392571d9612019f
Induranga FBD <83300979+FBID10@users.noreply.github.com>
2026-02-17 14:29:52 +0000

    Added Main

---
+++ main.py
@@ -0,0 +1,18 @@
+if __name__ == "__main__":
+    # 1. Collect the data
+    collector = ResourceCollector(region_name="us-east-1")
+    all_instances = collector.get_all_instances()
+
+    # 2. Define our rules
+    my_rules = ["Owner", "CostCenter", "Project"]
+    enforcer = TagEnforcer(required_tags=my_rules)
+
+    # 3. Find the violators
+    flagged_resources = enforcer.find_violators(all_instances)
+
+    # 4. Report the findings
+    print(f"Total Instances Scanned: {len(all_instances)}")
+    print(f"Total Violators Found: {len(flagged_resources)}")
+    
+    for v in flagged_resources:
+        print(f" Instance {v['InstanceId']} is missing: {v['MissingTags']}")
